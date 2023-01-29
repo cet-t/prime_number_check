233245bf0ae77fcce13a77068a94c5a030517c6c
cet-t <cets021@gmail.com>
2023-01-29 16:46:32 +0900

    first commit

---
+++ sosu.py
@@ -0,0 +1,23 @@
+﻿import random as r
+
+# num = int(input())
+num = r.randint(0, 50)
+nums: list[bool] = []
+nool: bool = True
+
+if type(num) is int:
+    print(f'input: {num}')
+
+    for i in range(2, num, 1):
+        print(i)
+
+        is_prime_num = num % i == 0
+        if is_prime_num:
+            nums.append(False)
+        else:
+            nums.append(True)
+
+    for i in nums:
+        print(i)
+
+    print(f'{num} is sosu-?: {all(nums)}')
