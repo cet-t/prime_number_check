d2fa8e287dd94e2b7551740d95a31e8ed6f3b0ef
cet-t <cets021@gmail.com>
2023-01-30 13:44:28 +0900

    wip

---
+++ src/sosu.py
@@ -0,0 +1,23 @@
+﻿import random as r
+import tkinter as tk
+
+# num = int(input())
+# 10000個目の素数が104729
+# num = r.randint(0, 104729)
+num = 57
+nums: list[bool] = []
+
+print(f'input: {num}')
+if num < 2:
+    pass
+
+for i in range(2, num, 1):
+    is_prime = num % i == 0
+    if is_prime:
+        nums.append(False)
+    else:
+        nums.append(True)
+    pass
+
+
+print(f'{num} is sosu-?: {all(nums)}')
---
+++ src/tktest.py
@@ -0,0 +1,37 @@
+﻿import tkinter as tk
+import const as c
+
+c.W_X = 300
+c.W_Y = 200
+
+rt = tk.Tk()
+rt.geometry(f'{c.W_X}x{c.W_Y}')
+rt.title('これそすー…？')
+
+box_lbl = tk.Label(text='数値')
+box_lbl.place(x=132, y=60)
+
+# txt box
+num = tk.Entry(width=20)
+num.place(x=90, y=80)
+
+btn_comp = tk.Button(text="判定")
+btn_comp.place(x=130, y=100)
+
+judge_txt = tk.Label(text='!そすー')
+judge_txt.place(x=128, y=130)
+
+
+def btn_click():
+    n = int(num.get())
+    judge_txt.insert(n)
+
+# n: int = txt_input.get()
+# if n != None:
+#     tk.l = tk.Label(text=n).place(x=0, y=0)
+
+
+btn_click()
+
+
+rt.mainloop()
